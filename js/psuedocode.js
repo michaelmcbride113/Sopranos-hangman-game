39b9f9c1180744f550c3900a7b4b5b77d097a2df
Michael McBride <michaelmcbride113@gmail.com>
2025-02-03 17:07:28 -0500

    updated data.js file, connected html and added boilerplate

---
+++ js/psuedocode.js
@@ -0,0 +1,104 @@
+// Sopranos hangman
+
+// Schedule:
+// Monday
+//  - Finish before coding steps
+// - Build Java Dataset
+// - HTML
+// Tuesday
+//  - finish HTML
+//  - Begin JS
+// Wednesday
+//  -- JS
+// Thursday
+//  - Finish JS
+// Friday
+//  - CSS
+// - Nice to haves
+//  - Presentation
+//  - After coding steps
+// Weekend
+//  - After coding steps
+
+
+// Before coding
+
+// Write psuedocode+get approval - done
+// Create github repo - done
+// Name project something functional, not GA Project 1 for example - done
+// Pull file down to VS code on my machine, create app.js, data.js, index.html 
+// and make directory for CSS and put ./css/style.css in this directory - done
+// Create psuedocode to track bugs that need fixing - done
+
+
+
+
+// Code:
+
+// HTML
+
+// Build hangman podium
+// Write instructions for playing the game
+// Build selectable keyboard on screen
+// Create section for the word or phrase to populate
+// Create instructions for the game
+// If the user gets the answer wrong, display the correct word/phrase/name on screen
+// Assign IDs and Classes to HTML
+
+
+
+// CSS
+
+// Use flexbox or grid for page layout
+// Ensure website style is up to WCAG 2.0 evel AA standard
+// Style keyboard
+// Style hangman podium
+// Build the actual hangman (ideally using edited photo of Tony)
+// Place hangman pieces if incorrect answer
+
+
+
+// JS
+
+// create winning logic
+// create losing logic
+// Make letter buttons work on screen
+// When a letter is chosen correctly, have the letter populate in the section where the word appears
+// Place hangman pieces if incorrect answer
+// Create reset logic
+// Create clear+start over button
+// Display the correct answer if the answer is wrong
+// write code to have the hangman show up piece by piece - max 7 incorrect selections
+
+
+
+
+// JS Data
+
+// Find list of expressions, people, locations etc.. 
+// online and create a dataset for the game - done
+// Possibly create multiple datasets based on type  
+// and use the name of the dataset as a hint in the game
+
+// Nice to haves
+// Style website like website in the show https://sopranos.fandom.com/wiki/Www.megamob.com?file=1_4_sopranos_2.jpg
+// Fix for mobile and different screen sizes
+// Allow users to type answers from their keyboard (Key presses)
+
+
+
+
+
+// After coding
+
+// Ensure the game is up to the WCAG 2.0 level AA standards
+// Ensure images on the site have alt text
+// Commit to github when adequite progress has been made
+// Clean up code to project requirements
+// format code to project requirements
+// Write README based on project requirements
+// One final runthrough of the project requirements
+// Create project presentation
+// Meet with Ben or Ashley to run through the completed project if that's allowed
+// Commit final completed project
+// Deploy project when completed
