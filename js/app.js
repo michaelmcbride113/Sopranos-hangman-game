bd59a7b059b69db929f270b7593d6f71acbae1ee
Michael McBride <michaelmcbride113@gmail.com>
2025-02-05 13:57:21 -0500

    Added game winning logic and additional querySelectors. Half fixed keboardchoice query selector, will finish after break.

--- js/app.js
+++ js/app.js
@@ -1,22 +1,26 @@
 // Bug tracking
-
+// Fixed keyboardChoiceEl to target all keys but now need to fix query selector to target all keys
 
 /*-------------------------------- Constants --------------------------------*/
 
 
 /*-------------------------------- Variables --------------------------------*/
-let winner = false;
-let answer = '';
-let gameWord = names[Math.floor(Math.random () * names.length)];
+let gameOver = false;
+let guessedWord = names[Math.floor(Math.random () * names.length)];
+let guessedLetters = [];
 let remainingGuesses = 7;
+let displayWord = [];
 
 /*------------------------ Cached Element References ------------------------*/
-const keyboardChoice = document.querySelector('.keys')
-const startGameBtn = document.querySelector('#start-game')
-const resetGameBtn = document.querySelector('#reset-game')
+const keyboardChoiceEl = document.querySelectorAll('.keys');
+const startGameBtn = document.querySelector('#start-game');
+const resetGameBtn = document.querySelector('#reset-game');
+const wordDisplayEl = document.querySelector('#spaces');
+const messageEl = document.querySelector('#message');
+
 
 /*----------------------------- Event Listeners -----------------------------*/
-keyboardChoice.addEventListener('click', () => {
+keyboardChoiceEl.addEventListener('click', () => {
     console.log('i am clicked');
 })
 startGameBtn.addEventListener('click', () => {
@@ -27,18 +31,25 @@ startGameBtn.addEventListener('click', () => {
 
 /*-------------------------------- Functions --------------------------------*/
 const init = () => {
-    let answer = '';
-    let gameWord = names[Math.floor(Math.random () * names.length)];
-    let winner = false;
-    let remainingGuesses = 7;
+    gameOver = false;
+    guessedWord = names[Math.floor(Math.random () * names.length)];
+    guessedLetters = [];
+    remainingGuesses = 7;
+    displayWord = [];
     render();
 };
 const render = () => {};
 
 
 const checkForWinner = () => {
-
-}
+    if (displayWord.every(letter => letter !== '_')) {
+        messageEl.textContent = 'Congrats, you win!';
+        gameOver = true;
+    } else if (remainingGuesses = 0) {
+        messageEl.textContent = `You lost! Your word was ${guessedWord}`
+        gameOver = true;
+    }
+};
 
 resetGameBtn.addEventListener('click', init);
 init();
