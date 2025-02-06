9e8cc7c528810798a910bbe4f23979b5d59f4240
Michael McBride <michaelmcbride113@gmail.com>
2025-02-06 17:49:37 -0500

    fixed bugs, 2 more things to add in JS then on to CSS

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
 // Bug tracking
-// Code seems to be completed - debug fest tomorrow
-// Only message displaying in "message" h2 is congrats you win
-// the spaces have disapeared on the webpage - maybe have spaces come from JS
-// Add another div or span for remaining guesses
-// add query selector for remaining guesses
+// Fix spaces with the _
+// Add pieces with incorrect guess
+// Update message when reseting game
 
 /*-------------------------------- Constants --------------------------------*/
 const init = () => {
@@ -29,7 +27,7 @@ const keyClick = (event) => {
     const guessedLettersFilter = guessedLetters.filter((guessedLetter) => 
         guessedLetter === letter);
     if(guessedLettersFilter.length > 0) {
-        messageEl.textContent = `You already guesses ${letter}, please make another selection`;
+        messageEl.textContent = `You already guessed ${letter}, please make another selection`;
         return;
     }
     guessedLetters.push(letter);
@@ -64,7 +62,7 @@ const resetGameBtn = document.querySelector('#reset-game');
 const shownWordEl = document.querySelector('#spaces');
 const messageEl = document.querySelector('#message');
 const remainingGuessesEl = document.querySelector('#remaining-guesses'); 
-
+const guessedLettersEl = document.querySelector('#guessed-letters'); 
 
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -77,26 +75,10 @@ resetGameBtn.addEventListener('click', init);
 
 
 /*-------------------------------- Functions --------------------------------*/
-// const init = () => {
-//     gameOver = false;
-//     guessedWord = names[Math.floor(Math.random() * names.length)];
-//     guessedLetters = [];
-//     shownWord = [];
-    
-//     for (let i=0; i < guessedWord.length; i++) {
-//         if (guessedWord[i] === ' ') {
-//             shownWord.push(' ');
-//         } else {
-//             shownWord.push('_');
-//         }
-//     }
-//     remainingGuesses = 7;
-//     render();
-// };
 const render = () => {
-    shownWordEl.textContent = '';
     shownWordEl.textContent = shownWord.join(' '); // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
     remainingGuessesEl.textContent = `${remainingGuesses} guesses remaining`;
+    guessedLettersEl.textContent = `Letters guessed: ${guessedLetters} `;
     checkForWinner();
 };
 
@@ -110,7 +92,6 @@ const checkForWinner = () => {
     }
 };
 
-// resetGameBtn.addEventListener('click', init);
 init();
 
 
